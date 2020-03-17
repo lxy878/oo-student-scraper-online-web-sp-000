9bdbf19e4d9aee390f807527c380ac812114bfaa
Xiaoyi Liu <lxy878@gmail.com>
2020-03-17 21:33:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,7 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url)).css("div.main-wrapper.profile")
-    url =  doc.css("div.social-icon-container a")
+    url =  doc.css("div.social-icon-container a").first
     # profile_quote = doc.css("div.profile-quote").text
     # bio = doc.css("div.description-holder p").text
     a = doc.first
