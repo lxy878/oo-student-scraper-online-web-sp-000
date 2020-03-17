83750174003a27cf7370452df1be153ef883046d
Xiaoyi Liu <lxy878@gmail.com>
2020-03-17 20:25:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url)).css("div.roster-cards-container")
+    a = doc.first
     array = []
     binding.pry
   end
