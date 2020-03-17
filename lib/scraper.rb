c8f737c7a1eaaaf43b70e1408b7b4ea9b6313102
Xiaoyi Liu <lxy878@gmail.com>
2020-03-17 20:15:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,9 +4,9 @@ require 'Nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    url = open(index_url)
+    html = Nokogiri::HTML(open(index_url))
     array = []
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
