3c44a76b9f31a26d9f89549a0249f1093dd2be75
Xiaoyi Liu <lxy878@gmail.com>
2020-03-17 20:37:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,10 +6,16 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url)).css("div.student-card")
-    a = doc.first
     # name = doc.css("h4.student-name").text
     # location = doc.css("p.student-location").text
     array = []
+    doc.each do |element|
+      name = doc.css("h4.student-name").text
+      new_hash = {
+        :name => name,
+        :location => 
+      }
+    end
     binding.pry
   end
 
